42556b3fa94b2af4ce8bc0eedbc129051d5f2a9c
Rita Moura <45463793+ritamoura96@users.noreply.github.com>
2024-02-23 14:52:11 +0000

    Update utils_train.py

--- utils_train.py
+++ utils_train.py
@@ -1,30 +1,25 @@
-import argparse
-import math as m
 import numpy as np
-import pandas as pd
-import seaborn as sns
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 from xgboost import XGBRegressor
+from nn_architecture import ANN_model
 from scikeras.wrappers import KerasRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 
-from nn_architecture import ANN_model
+
 
 def load_models_to_train(X_train, y_train):
 
     models = [
-        {
-        'name': 'DT', 'model': DecisionTreeRegressor(),
+        {'name': 'DT', 'model': DecisionTreeRegressor(), 'parameters': {},
+        #'name': 'DT', 'model': DecisionTreeRegressor(),
         #'parameters': {'splitter': ['best', 'random'], 'max_depth': [5, 10, 50, 100, 200, None],
         #'min_samples_leaf': [0.3, 0.5, 1, 2, 3], 'min_samples_split': [0.5, 0.7, 1.0, 2]},
         #'name': 'RF', 'model': RandomForestRegressor(), 'parameters': {'n_estimators': [200], 'min_samples_leaf': [1], 'min_samples_split': [2]},
         #'name': 'XGBT', 'model': XGBRegressor(), 'parameters': {'max_depth': [15], 'multi_strategy': ['multi_output_tree']},
-
         #'name': 'SVR', 'model': SVR(), 'parameters': {'C': [3.0], 'gamma': ['scale'], 'kernel': ['rbf']},
         #'name': 'ANN', 'model': KerasRegressor(build_fn=ANN_model, verbose=1, learning_rate=None, activation_function=None,
         #                        num_neurons1=None, num_neurons2=None, num_neurons3=None, x_train=X_train, case=None),
@@ -37,15 +32,15 @@ def load_models_to_train(X_train, y_train):
     return models
 
 
-def data_pre_processing(df, number_nodes, number_frames, features, coords, target, case):
+def data_pre_processing(df, number_nodes, number_frames):
 
   # get features from dataset
-  X = df.iloc[:, [0, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+  X = df.iloc[:, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]]
 
   # get target from dataset
-  y = df.iloc[:, [11, 12, 13]]
+  y = df.iloc[:, [12, 13, 14]]
 
-  # stratification 
+  # stratification
   X_feature = df.iloc[:, [0] + list(range(5, 9))]
   X_feature = X_feature.groupby('simulation').mean().values
 
@@ -55,15 +50,15 @@ def data_pre_processing(df, number_nodes, number_frames, features, coords, targe
 
   # define % of data do train
   data_size = 0.1
-  np.random.seed(42) 
+  np.random.seed(42)
   random_indices = np.random.choice(len(X), size=int(data_size * len(X)), replace=False)
   X = X[random_indices, :]
   y = y[random_indices, :]
   X_feature = X_feature[random_indices, 0]
 
   # definition of % of data for test set
-  test_split = 0.1 
-  # train test split with stratification 
+  test_split = 0.1
+  # train test split with stratification
   splitter = StratifiedShuffleSplit(n_splits=5, test_size=test_split, random_state=42)
 
   for train_index, test_index in splitter.split(np.zeros(X_feature.shape[0]), X_feature):
@@ -77,7 +72,7 @@ def data_pre_processing(df, number_nodes, number_frames, features, coords, targe
   y_train = np.vstack(y_train)
   y_test = np.vstack(y_test)
 
-  # data scaling 
+  # data scaling
   scaler = StandardScaler()
   scaler_fit = scaler.fit(X_train)
   X_train = scaler_fit.transform(X_train)
@@ -90,4 +85,3 @@ def make_predictions(X_test, scaler_fit, model_fit):
     all_predictions = model_fit.predict(scaler_fit.transform(X_test))
 
     return all_predictions
-
