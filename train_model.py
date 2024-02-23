2637d6c03ea59440c95d043744507251990cb129
Rita Moura <45463793+ritamoura96@users.noreply.github.com>
2024-02-23 14:20:19 +0000

    Train model
    Different stages of training a machine learning model from finite element simulation data

--- train_model.py
+++ train_model.py
@@ -1,43 +1,50 @@
 import pandas as pd
 from utils_train import *
 
+df = pd.read_pickle('dataset.pkl')
 
-df = pd.read_pickle('dataset_length.pkl')
 number_nodes = len(df['node_number'].unique())
 number_frames = len(df['time'].unique())
+number_simulations = len(df['simulation'].unique())
+simulation_len = number_nodes * number_frames
 
-# Stratified train test split
-X_train, X_test, y_train, y_test = split_train_test(df, number_nodes, number_frames)
+X_train, y_train, X_test, y_test, scaler_fit = data_pre_processing(df, number_nodes, number_frames, features, coords, target, case)
 
-# Scaling
-X_train, X_test = scaling_data(X_train, X_test)
-
-# Load models to train
 models_to_train = load_models_to_train(X_train, y_train)
+
 results = []
 
 for model in models_to_train:
 
-    # Get model and parameters
     m = model.get('model')
     name = model.get('name')
     params = model.get('parameters')
+    
+    # define cross validation
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-    # Cross-validation
-    model_fit = hyperparameter_opt_cv(m, params, X_train, y_train)
+    # use grid search method
+    grid = GridSearchCV(m, params, cv=kf, scoring='neg_mean_absolute_error', verbose=10, n_jobs=16)
 
-    # Test
-    mse, mse_x, mse_y, mse_z, mae, mae_x, mae_y, mae_z = test_model(X_test, y_test, model_fit)
+    # fit model
+    grid.fit(X_train, y_train)
+    model_fit = grid.best_estimator_
 
-    # Save model
-    # dump(model_fit, f'/models/{name}.joblib')
+    # save best model
+    joblib.dump(model_fit, f'{name}.joblib')
 
-    # Output results
-    model_results = [name, mse, mse_x, mse_y, mse_z, mae, mae_x, mae_y, mae_z]
+    # predict 
+    all_predictions = make_predictions(case, name, X_test, scaler_fit, model_fit, simulation_len, number_frames, number_nodes, time_step, coords, features)
+
+    # evaluate 
+    mae = round(mean_absolute_error(y_test, all_predictions), 3)
+    mae_x = round(mean_absolute_error(y_test[:, 0], all_predictions[:, 0]), 3)
+    mae_y = round(mean_absolute_error(y_test[:, 1], all_predictions[:, 1]), 3)
+    mae_z = round(mean_absolute_error(y_test[:, 2], all_predictions[:, 2]), 3)   
+    mape = round(mean_absolute_percentage_error(y_test, all_predictions), 6)
+
+    model_results = [name, mae, mae_x, mae_y, mae_z, mape]
     results.append(model_results)
 
-df_results = pd.DataFrame(data=results, columns=['Model', 'MSE', 'MSE_x', 'MSE_y', 'MSE_z',
-                                                 'MAE', 'MAE_x', 'MAE_y', 'MAE_z'])
-df_results.to_csv(f'train_results.csv')
-
-print()
+df_results = pd.DataFrame(data=results, columns=['Model', 'MAE', 'MAE_x', 'MAE_y', 'MAE_z', 'MAPE'])
+df_results.to_csv(f'results.csv')
