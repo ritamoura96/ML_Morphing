d78e5f7a7d082e124d421bba44aadb2962ee6f87
Rita Moura <45463793+ritamoura96@users.noreply.github.com>
2024-02-23 14:51:33 +0000

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,4 +1,8 @@
+import joblib
 import pandas as pd
+from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+
 from utils_train import *
 
 df = pd.read_pickle('dataset.pkl')
@@ -8,18 +12,17 @@ number_frames = len(df['time'].unique())
 number_simulations = len(df['simulation'].unique())
 simulation_len = number_nodes * number_frames
 
-X_train, y_train, X_test, y_test, scaler_fit = data_pre_processing(df, number_nodes, number_frames, features, coords, target, case)
+X_train, y_train, X_test, y_test, scaler_fit = data_pre_processing(df, number_nodes, number_frames)
 
 models_to_train = load_models_to_train(X_train, y_train)
 
 results = []
 
 for model in models_to_train:
-
     m = model.get('model')
     name = model.get('name')
     params = model.get('parameters')
-    
+
     # define cross validation
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
@@ -33,14 +36,14 @@ for model in models_to_train:
     # save best model
     joblib.dump(model_fit, f'{name}.joblib')
 
-    # predict 
-    all_predictions = make_predictions(case, name, X_test, scaler_fit, model_fit, simulation_len, number_frames, number_nodes, time_step, coords, features)
+    # predict
+    all_predictions = make_predictions(X_test, scaler_fit, model_fit)
 
-    # evaluate 
+    # evaluate
     mae = round(mean_absolute_error(y_test, all_predictions), 3)
     mae_x = round(mean_absolute_error(y_test[:, 0], all_predictions[:, 0]), 3)
     mae_y = round(mean_absolute_error(y_test[:, 1], all_predictions[:, 1]), 3)
-    mae_z = round(mean_absolute_error(y_test[:, 2], all_predictions[:, 2]), 3)   
+    mae_z = round(mean_absolute_error(y_test[:, 2], all_predictions[:, 2]), 3)
     mape = round(mean_absolute_percentage_error(y_test, all_predictions), 6)
 
     model_results = [name, mae, mae_x, mae_y, mae_z, mape]
